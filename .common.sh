10eed89a7c84461020d8226f24c4ffa95a1aac6e
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-03-23 05:19:49 +0000

    update

---
+++ .common.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH="$HOME/bin:$PATH"
+
+CHECK_OUTPUT="$HOME/.check_update_output.txt"
+
+if [ -s $CHECK_OUTPUT ]; then
+    cat $CHECK_OUTPUT
+    echo '' >  $CHECK_OUTPUT
+fi
+
+check_update "$HOME/bin" --bg "$CHECK_OUTPUT"
+check_update "$HOME/dotfiles" --bg "$CHECK_OUTPUT"
+background "sync_dotfiles d" /dev/null
+
+source .common_aliases.sh
+
