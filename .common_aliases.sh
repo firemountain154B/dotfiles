5c37202f5739d4c2a21430fe56dbe889d2bfa30a
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-03-23 05:08:43 +0000

    update

--- .common_aliases.sh
+++ .common_aliases.sh
@@ -5,8 +5,6 @@
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias cpv='rsync -ah --info=progress2'
-alias cpz='rsync -ahz --info=progress2'
 
 # List directory contents
 alias la='ls -a'
@@ -19,21 +17,8 @@ alias path='echo $PATH'
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
-alias rm="rm -i"
-alias cp="cp -i"
 
 # Show active network interfaces
 alias ifactive="sudo lsof -i -P | grep -i 'python'"
 alias ghist="history | grep"
-
-alias G='| grep'
-alias H='| head'
-alias L='| less'
-alias LL='2>&1 | less'
-alias M='| most'
-alias NE='2> /dev/null'
-alias NUL='> /dev/null 2>&1'
-alias P='2>&1| pygmentize -l pytb'
-alias T='| tail'
-alias cp='cp -i'
 
