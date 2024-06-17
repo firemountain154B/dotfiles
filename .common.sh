8c322e1e5e1bef38849be17969c83d20cabe0012
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-06-17 06:42:25 +0100

    fix .common.sh

--- .common.sh
+++ .common.sh
@@ -88,10 +88,10 @@ export PATH="$HOME/bin:$PATH"
 
 CHECK_OUTPUT="$HOME/.check_update_output.txt"
 
-if [ -s $CHECK_OUTPUT ]; then
-    cat $CHECK_OUTPUT
-    echo '' >  $CHECK_OUTPUT
-fi
+[ -e $CHECK_OUTPUT ] || touch $CHECK_OUTPUT
+
+cat $CHECK_OUTPUT
+echo '' >  $CHECK_OUTPUT
 
 check_update "$HOME/bin" --bg "$CHECK_OUTPUT"
 check_update "$HOME/dotfiles" --bg "$CHECK_OUTPUT"
