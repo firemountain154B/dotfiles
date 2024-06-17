50ee542fad20906bf8e8438fba71c4660e9a5fba
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-06-17 06:29:18 +0100

    update .common.sh .gitconfig

--- .common.sh
+++ .common.sh
@@ -22,7 +22,6 @@ check_color_support() {
 ncolors=$(tput colors)
 if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
 # Common aliases for both Bash and Zsh
-echo "here"
 alias grep='grep --color=auto'
 alias diff='diff --color=auto'
 alias ip='ip -color=auto'
@@ -60,8 +59,22 @@ alias la='ls -a'
 alias ll='ls -lh'
 alias l='ls -lAh'
 
-# Print path
-alias path='echo $PATH'
+# continue wget
+alias wget='wget -c'
+
+alias df='df -H'
+alias du='du -ch'
+
+## pass options to free ##
+alias meminfo='free -m -l -t'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
 
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
@@ -83,4 +96,16 @@ fi
 check_update "$HOME/bin" --bg "$CHECK_OUTPUT"
 check_update "$HOME/dotfiles" --bg "$CHECK_OUTPUT"
 background "sync_dotfiles d" /dev/null
+
+if [ $current_shell = "bash" ] ; then
+  git_branch() {
+    branch=$(git branch 2>/dev/null | grep '*' | sed 's/* //')
+    if [ -n "$branch" ]; then
+      echo " ($branch)"
+    fi
+  }
+
+  PS1='$(if [ $? -ne 0 ]; then echo -e "\[\033[31m\]? "; fi)\[\033[32m\]\t \[\033[1;34m\]\W\[\033[0m\]$(git_branch)\$ '
+fi
+
 
