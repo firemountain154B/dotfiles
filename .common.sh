f99b9fe73711dd6204599e6d716bf953d50a3c5a
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-06-26 01:17:17 +0100

    update

--- .common.sh
+++ .common.sh
@@ -1,105 +1,68 @@
 #!/bin/bash
 
+export PATH="$HOME/bin:$PATH"
 current_shell=$(ps -p $$ -ocomm=)
 
 
-# Define aliases based on color support
-ncolors=$(tput colors)
-if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
-# Common aliases for both Bash and Zsh
-alias grep='grep --color=auto'
-alias diff='diff --color=auto'
-alias ip='ip -color=auto'
-
-# Git with color output
-alias git='git -c color.ui=auto'
-
-# Colorized JSON output (requires jq)
-alias json='jq .'
-
-alias ls='ls --color=auto'
-alias dir='dir --color=auto'
-
-# Syntax highlighting for various file types using bat (if installed)
-if command -v bat >/dev/null 2>&1; then
-  alias cat='bat --theme=TwoDark'
-  alias less='bat --theme=TwoDark --pager="less -R"'
-fi
-fi
-
-# Easier navigation
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-
-# List directory contents
-alias la='ls -a'
-alias ll='ls -lh'
-alias l='ls -lAh'
-
-# continue wget
-alias wget='wget -c'
-
-alias df='df -H'
-alias du='du -ch'
-
-## pass options to free ##
-alias meminfo='free -m -l -t'
-
-## get top process eating memory
-alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-
-## get top process eating cpu ##
-alias pscpu='ps auxf | sort -nr -k 3'
-alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
-
-# IP addresses
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ipconfig getifaddr en0"
-
-# Show active network interfaces
-alias ifactive="sudo lsof -i -P | grep -i 'python'"
-alias ghist="history | grep"
-
-export PATH="$HOME/bin:$PATH"
-
-CHECK_OUTPUT="$HOME/.check_update_output.txt"
-
-[ -e $CHECK_OUTPUT ] || touch $CHECK_OUTPUT
-
-cat $CHECK_OUTPUT
-echo '' >  $CHECK_OUTPUT
-
-check_update "$HOME/bin" --bg "$CHECK_OUTPUT"
-check_update "$HOME/dotfiles" --bg "$CHECK_OUTPUT"
-background "sync_dotfiles d" /dev/null
-
-##################################################
-# bash specific
 if [ $current_shell = "bash" ] ; then
-  git_branch() {
-    branch=$(git branch 2>/dev/null | grep '*' | sed 's/* //')
-    if [ -n "$branch" ]; then
-      echo " ($branch)"
-    fi
-  }
+   
+# BASH SPECIFIC
 
-  PS1='$(if [ $? -ne 0 ]; then echo -e "\[\033[31m\]?"; fi)\[\033[32m\]\h \[\033[1;34m\]\W\[\033[0m\]$(git_branch) '
+PS1='$(if [ $? -ne 0 ]; then echo -e ${RED}; else echo -e ${GREEN}; fi)→ ${BLUE}\W ${RESET} $(git branch &>/dev/null | grep "^\*" | sed "s/^\* //")'
+
+elif [ $current_shell = "-zsh" ] ; then
+   
+# ZSH SPECIFIC
+
+setopt CORRECT
+setopt CORRECT_ALL
+setopt NO_CASE_GLOB
+
+HISTFILE="${HOME}/.zsh_history"
+HISTSIZE=2000
+SAVEHIST=5000
+# share history across multiple zsh sessions
+setopt SHARE_HISTORY
+# append to history
+setopt APPEND_HISTORY
+# expire duplicates first
+setopt HIST_EXPIRE_DUPS_FIRST
+# do not store duplications
+setopt HIST_IGNORE_DUPS
+#ignore duplicates when searching
+setopt HIST_FIND_NO_DUPS
+# removes blank lines from history
+setopt HIST_REDUCE_BLANKS
+autoload -Uz compinit && compinit 
+
+PROMPT='%(?..%F{red}?%? )%B%F{lime}%1~ %f'
+
 fi
 
-##################################################
-# mac specific
 if [ $(uname) = "Darwin" ] ; then
   export CLICOLOR=1
   export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
 fi
 
-
-##################################################
-# bash specific
-if [ $current_shell = "-zsh" ] ; then
-PROMPT='%(?..%F{red}?%? )%F{green}%m %{$fg_bold[blue]%}%1~%{$reset_color%} $(git_prompt_info)'
+ncolors=$(tput colors)
+if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
+alias grep='grep --color=auto'
+alias diff='diff --color=auto'
+alias ip='ip -color=auto'
+alias git='git -c color.ui=auto'
+alias json='jq .'
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
 fi
 
 
+source ${HOME}/.aliases
+
+CHECK_OUTPUT="$HOME/.check_update_output.txt"
+[ -e $CHECK_OUTPUT ] || touch $CHECK_OUTPUT
+cat $CHECK_OUTPUT
+cat /dev/null > $CHECK_OUTPUT
+
+check_update "$HOME/bin" --bg "$CHECK_OUTPUT"
+check_update "$HOME/dotfiles" --bg "$CHECK_OUTPUT"
+background "sync_dotfiles d" /dev/null
---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+source {$HOME}/.aliases
+export CLICOLOR=1
+export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
+
