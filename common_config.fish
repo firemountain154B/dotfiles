412f2cc5119819fa566c6edf9bab9c4c426d017b
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-07-30 17:14:16 +0200

    update

--- common_config.fish
+++ common_config.fish
@@ -7,10 +7,5 @@ export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
 contains {$HOME}/dotfiles $fish_user_paths; or set -Ua fish_user_paths {$HOME}/dotfiles
 contains {$HOME}/dotfiles/bin $fish_user_paths; or set -Ua fish_user_paths {$HOME}/dotfiles/bin
 
-set CHECK_OUTPUT "$HOME/.check_update_output.txt"
-[ -e $CHECK_OUTPUT ] || touch $CHECK_OUTPUT
-cat $CHECK_OUTPUT
-cat /dev/null > $CHECK_OUTPUT
-check_update "$HOME/dotfiles" --bg "$CHECK_OUTPUT"
-background "sync_dotfiles d" $CHECK_OUTPUT
+background_update
 
