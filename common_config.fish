bd38c8d9dddf73c7bd4d95ac937abed8787aa4e0
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-11-27 21:39:15 +0000

    add MAKE config

--- common_config.fish
+++ common_config.fish
@@ -3,6 +3,7 @@
 source {$HOME}/dotfiles/common_aliases
 export CLICOLOR=1
 export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
+export MAKE='make -j9'
 
 contains {$HOME}/dotfiles $fish_user_paths; or set -Ua fish_user_paths {$HOME}/dotfiles
 contains {$HOME}/dotfiles/bin $fish_user_paths; or set -Ua fish_user_paths {$HOME}/dotfiles/bin
